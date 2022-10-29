ebde100707e743f03f018f6268100bfe67fa411a
lazizern <laziz.ibodullaev.82@gmail.com>
2022-10-29 09:53:09 -0400

    StringReverse

--- src JDUS1/day14_forLoop/StringReverse.java
+++ src JDUS1/day14_forLoop/StringReverse.java
@@ -3,4 +3,18 @@ package day14_forLoop;
 public class StringReverse {
 
 
+    public static void main(String[] args) {
+
+        String str = "Cydeo School";
+        //            0123
+        //"avaJ"
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        System.out.println(reverse);
+    }
 }
