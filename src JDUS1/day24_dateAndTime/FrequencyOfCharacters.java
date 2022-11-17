85a5da8089fd5e5d02fe35b30192b0f0c2d4baa0
lazizern <laziz.ibodullaev.82@gmail.com>
2022-11-17 16:04:23 -0500

    day24Updated

--- src JDUS1/day24_dateAndTime/FrequencyOfCharacters.java
+++ src JDUS1/day24_dateAndTime/FrequencyOfCharacters.java
@@ -1,6 +1,28 @@
 package day24_dateAndTime;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class FrequencyOfCharacters {
+
+    public static void main(String[] args) {
+
+
+        String str = "aabbbcccc";
+
+        String res = "";
+
+        for (String each : str.split("")) {
+            int frequency = Collections.frequency(Arrays.asList(str.split("")), each);
+
+            if (!res.contains(each)) {
+                res += each + frequency;
+            }
+        }
+
+        System.out.println(res);
+
+    }
 
 
 }
